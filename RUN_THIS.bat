42550271faadf389faa83c134a9c907b9fb928f9
Ethan Dagnone <ethandagnone@gmail.com>
2025-11-29 16:34:30 -0500

    Initial commit

---
+++ RUN_THIS.bat
@@ -0,0 +1,5 @@
+@echo off
+cd %~dp0
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+powershell.exe -executionpolicy bypass -file ./w11-up-regedit.ps1 && exit
